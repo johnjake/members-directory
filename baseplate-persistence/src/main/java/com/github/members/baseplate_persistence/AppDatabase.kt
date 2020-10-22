c22181e3363e2307223f49d26f7b2966e266ac74
John Jake <johnjake_talledo2000@yahoo.com>
2020-10-22 15:23:18 +0800

    RemoteMediator

--- baseplate-persistence/src/main/java/com/github/members/baseplate_persistence/AppDatabase.kt
+++ baseplate-persistence/src/main/java/com/github/members/baseplate_persistence/AppDatabase.kt
@@ -4,16 +4,18 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.github.members.baseplate_persistence.dao.MembersDao
+import com.github.members.baseplate_persistence.dao.RemoteKeysDao
 import com.github.members.baseplate_persistence.model.DBMembers
 
 
 @Database(
     entities = [DBMembers::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 
 @TypeConverters
 abstract class AppDatabase : RoomDatabase() {
     abstract fun membersDao(): MembersDao
+    abstract fun remoteKeysDao(): RemoteKeysDao
 }
